c4ed9e6b645cb62c4be093e9e99f0799f7ae7b35
bunnam988 <107185904+bunnam988@users.noreply.github.com>
2026-01-21 10:01:59 +0000

    fw_download_check library

---
+++ source/FwDownloadChk/fw_download_check.h
@@ -0,0 +1,14 @@
+#ifndef FW_DOWNLOAD_CHECK_H
+#define FW_DOWNLOAD_CHECK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int can_proceed_fw_download(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FW_DOWNLOAD_CHECK_H */
